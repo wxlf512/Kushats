0517b59e10566ba8efe50b85a0f37fcd2ba6cfe8
wxlf512 <wxlf512@gmail.com>
2023-06-12 15:33:18 +0300

    Added bottom navigation view

--- app/src/main/java/dev/wxlf/kushats/MainActivity.kt
+++ app/src/main/java/dev/wxlf/kushats/MainActivity.kt
@@ -2,10 +2,27 @@ package dev.wxlf.kushats
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import dev.wxlf.kushats.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.fragmentContainer.id) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.navigationBar, navController)
     }
 }
